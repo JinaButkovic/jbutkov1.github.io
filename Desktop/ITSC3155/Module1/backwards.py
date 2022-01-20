74e3aaf82b174e1345ae3c7425f9ca79f1f5e10f
JinaButkovic <jinabutkovic@gmail.com>
2022-01-20 18:53:36 -0500

    Adding backwards.py

---
+++ Desktop/ITSC3155/Module1/backwards.py
@@ -0,0 +1,8 @@
+#Jina Butkovic
+#used https://www.educative.io/edpresso/how-do-you-reverse-a-string-in-python to learn how to 
+# print a string backwards. In this instance, it is telling the computer to iterate through the string but take a step backwards
+# each time, rather than reading it forwards.
+sentence = input('Enter a string: ')
+
+sentenceBack = sentence[::-1]
+print(sentenceBack)
